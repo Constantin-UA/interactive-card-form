731f6a3cd0e01ce195cff65e883bdf5d77ec9468
Constantin-UA <konstantinsh579@gmail.com>
2022-10-30 18:57:16 +0300

    all work

---
+++ script.js
@@ -0,0 +1,50 @@
+console.log('hello world');
+//to
+const cardNumber = document.querySelector('.interactive-card-front-number');
+const cardOwner = document.querySelector('.interactive-card-front-owner');
+const cardDate = document.querySelector('.interactive-card-front-date');
+const cardCvc = document.querySelector('.interactive-card-back');
+//from
+const form = document.querySelector('.interactive-card-form');
+const formOwner = document.querySelector('#name-owner');
+const formNumber = document.querySelector('#number-card');
+const formMonth = document.querySelector('#month');
+const formYear = document.querySelector('#year');
+const formCvc = document.querySelector('#cvc');
+const formButton = form.querySelector('.interactive-card-form-button');
+//complete section
+const complete = document.querySelector('.interactive-card-complete');
+const completeButton = complete.querySelector('.interactive-card-complete-button');
+
+//sections show fn
+const Show = (elem) => {
+	elem.classList.remove('hide');
+	elem.classList.add('show');
+};
+
+const Hide = (elem) => {
+	elem.classList.remove('show');
+	elem.classList.add('hide');
+};
+
+//form submit event
+
+form.addEventListener('submit', (event) => {
+	event.preventDefault();
+	cardNumber.innerHTML = formNumber.value;
+	cardOwner.innerHTML = formOwner.value;
+	cardDate.innerHTML = `${formMonth.value}/${formYear.value}`;
+	cardCvc.innerHTML = formCvc.value;
+	Hide(form);
+	Show(complete);
+});
+completeButton.addEventListener('click', (eve) => {
+	Hide(complete);
+	Show(form);
+});
+
+//form validator
+function validator(element, rule) {
+	if (element === rule) return true;
+	return false;
+}
